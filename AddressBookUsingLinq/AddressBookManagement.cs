bb445e88a0952aa9d0b6a405491209f2d50ff335
shivammathur4 <shivamm.4aug@gmail.com>
2021-05-31 10:55:33 +0530

    Added delete method

--- AddressBookUsingLinq/AddressBookManagement.cs
+++ AddressBookUsingLinq/AddressBookManagement.cs
@@ -9,23 +9,23 @@ namespace AddressBookUsingLinq
 {
     public class AddressBookManagement
     {
-        
-        public DataTable UpdatedContactDetails(DataTable dataTable)
-        {
-            var recordData = dataTable.AsEnumerable().Where(a => a.Field<string>("FirstName").Equals("Kylie")).FirstOrDefault();
-            recordData["state"] = "Vegas";
-            Console.WriteLine("***********UpdatedData***************");
-            Console.WriteLine("FirstName:- " + recordData.Field<string>("firstName"));
-            Console.WriteLine("lastName:- " + recordData.Field<string>("lastName"));
-            Console.WriteLine("Address:- " + recordData.Field<string>("address"));
-            Console.WriteLine("City:- " + recordData.Field<string>("city"));
-            Console.WriteLine("State:- " + recordData.Field<string>("state"));
-            Console.WriteLine("zip:- " + Convert.ToInt32(recordData.Field<int>("zip")));
-            Console.WriteLine("phoneNumber:- " + Convert.ToDouble(recordData.Field<Double>("phoneNumber")));
-            Console.WriteLine("eMail:- " + recordData.Field<string>("eMail"));
-            Console.WriteLine("***************");
 
-            return dataTable;
+        public DataTable DeletingContactFromTable(DataTable datatable)
+        {
+            DataTable dataTableupdated = datatable.AsEnumerable().Except(datatable.AsEnumerable().Where(r => r.Field<string>("firstName") == "Khloe" && r.Field<string>("lastName") == "Lamar")).CopyToDataTable();
+            foreach (var data in dataTableupdated.AsEnumerable())
+            {
+                Console.WriteLine("FirstName:- " + data.Field<string>("firstName"));
+                Console.WriteLine("lastName:- " + data.Field<string>("lastName"));
+                Console.WriteLine("Address:- " + data.Field<string>("address"));
+                Console.WriteLine("City:- " + data.Field<string>("city"));
+                Console.WriteLine("State:- " + data.Field<string>("state"));
+                Console.WriteLine("zip:- " + Convert.ToInt32(data.Field<int>("zip")));
+                Console.WriteLine("phoneNumber:- " + Convert.ToDouble(data.Field<Double>("phoneNumber")));
+                Console.WriteLine("eMail:- " + data.Field<string>("eMail"));
+                Console.WriteLine("***************");
+            }
+            return dataTableupdated;
         }
     }
 }
