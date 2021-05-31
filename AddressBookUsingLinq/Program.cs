9696f395aca164d7114c78cfba3a083769ec86c3
shivammathur4 <shivamm.4aug@gmail.com>
2021-05-31 10:44:49 +0530

    Refactor program.cs

--- AddressBookUsingLinq/Program.cs
+++ AddressBookUsingLinq/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace AddressBookUsingLinq
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, Welcome to Address Book Using Linq!");
+            DataTable addressBookTable = new DataTable();
         }
     }
 }
